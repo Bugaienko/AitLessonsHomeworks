0ddad064a649d223aba786a93ba56d8cd83787be
Sergii Bugaienko <sergiibugaienko@gmail.com>
2023-07-23 18:07:43 +0200

    gitignore update

---
+++ src/homework06/Task2.java
@@ -0,0 +1,15 @@
+package homework06;
+
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите 4-х значное число: ");
+        String numberInString = scanner.nextLine();
+
+        System.out.println(numberInString);
+
+    }
+}
