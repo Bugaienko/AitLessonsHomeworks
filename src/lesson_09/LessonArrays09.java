82f4956b3db4e49d3d17186d1da90ac6c589f7d5
Sergii Bugaienko <sergiibugaienko@gmail.com>
2023-07-28 13:08:14 +0200

    Update LessonArrays09.java

--- src/lesson_09/LessonArrays09.java
+++ src/lesson_09/LessonArrays09.java
@@ -1,5 +1,7 @@
 package lesson_09;
 
+import java.util.Arrays;
+
 public class LessonArrays09 {
     public static void main(String[] args) {
         int[] array;
@@ -40,8 +42,61 @@ public class LessonArrays09 {
         System.out.println("New value: " + digitIndex1New);
 
 
+        int lengthArray = myArray.length;
+        lengthArray = myArray.length;
+        System.out.println("Array length: " + lengthArray);
+
+        int counter = 0;
+        while (counter < myArray.length) {
+            if (myArray[counter] % 2 == 0) {
+            System.out.println("Четное число из массива myArray: " + myArray[counter]); // myArray[0] -> myArray[1] ... myArray[length -1]
+
+            }
+            counter++;
+        }
+        System.out.println(Arrays.toString(myArray));
+        System.out.println(Arrays.toString(strings1));
 
 
+
+        String string = "Abcdef";
+//        int strCounter = 0;
+//        while (strCounter < string.length() - 1) {
+//            System.out.println(string.charAt(strCounter));
+//            strCounter++;
+//        }
+//        System.out.println(string);
+
+        System.out.println("+++++++++++ Arrays ++++++++++++");
+
+
+        int[] myArray2 = {55, -15, 543, 10, 17, 56, 101, 0, 145};
+        int[] arraySubarray = Arrays.copyOf(myArray2,4);
+        int[] copyOfArray = Arrays.copyOf(myArray2, myArray2.length);
+        System.out.println(Arrays.toString(arraySubarray));
+
+        int[] arraySubarray2 = Arrays.copyOfRange(myArray2, 4, 7);
+        int[] arraySubarray3 = Arrays.copyOfRange(myArray2, 4, 7);
+        int[] arraySubarray4 = Arrays.copyOfRange(myArray2, 4, 100); // [17, 56, 101, 0, 145, 0, 0, 0, 0 ... 0] -> new int[100 - 4]
+        System.out.println("arraySubarray2 " + Arrays.toString(arraySubarray2) );
+        System.out.println("arraySubarray3 " + Arrays.toString(arraySubarray3));
+        System.out.println(arraySubarray4.length);
+
+        System.out.println(arraySubarray2 == arraySubarray3);
+        System.out.println(Arrays.equals(arraySubarray2, arraySubarray3));
+
+
+        System.out.println("myArray2 " + Arrays.toString(myArray2));
+        Arrays.sort(myArray2, 2, 6);
+
+        System.out.println("myArray2 " + Arrays.toString(myArray2));
+        Arrays.sort(myArray2);
+        System.out.println("myArray2 " + Arrays.toString(myArray2));
+
+        System.out.println("copyOfArray: " + Arrays.toString(copyOfArray));
+
+
+        int[] asdea = {343, 55, 535, 5,35, 34, 7};
 
     }
 }
