ecf355a10b479090d385b4c087f2c11542165fe8
Sergii Bugaienko <sergiibugaienko@gmail.com>
2023-07-31 12:59:18 +0200

    Update LoopFor.java

--- src/lesson_10/LoopFor.java
+++ src/lesson_10/LoopFor.java
@@ -52,12 +52,60 @@ public class LoopFor {
 
         int[] array = {5, 10, 43, 545, -10, -23, 0, 15};
 
+        /*
         for (int k = 0; k < array.length; k++) {
             System.out.print(array[k] + " ");
         }
         System.out.println();
+        */
 
+
+        /*
         // вывести на экран все числа от 5 до 20;
+        for (int k = 5; k <= 20; k++) {
+            System.out.println(k);
+        }
+        */
+
+        // int[] array = {5, 10, 43, 545, -10, -23, 0, 15};
+
+        System.out.println(" =============== ");
+        int forSearch = 545; // вывести на экран индекс элемента по заданному значению
+        int index = -1;
+
+        for (int k = 0; k < array.length; k++) {
+            if (array[k] == forSearch) {
+                index = k;
+                System.out.println("Совпадение найдено");
+                if (index == 3) {
+                    continue; //break
+                }
+                System.out.println("Else ");
+
+            } else {
+                System.out.println("число " + array[k] + " не равно " + forSearch);
+            }
+        }
+
+       System.out.println(index);
+        System.out.println();
+        System.out.println("==================");
+        // Распечатать 10 раз цифры от 0 до 4
+
+        for (int k = 0; k < 10; k++) {
+            System.out.print(k + ": ");
+            for (int l = 0; l < 5; l++) {
+                System.out.print(" test ");
+                if (k  == 3) break;
+                System.out.print(l + " ");
+            }
+            System.out.println();
+        }
+
+        int a = 5;
+        // a / 2 -> 2, остаток = 2
+        // boolean b = a % (2...9) == 0; //
+//        System.out.println(b);
 
     }
 }
