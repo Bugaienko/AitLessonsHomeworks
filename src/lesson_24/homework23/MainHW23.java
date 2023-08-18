48c7f627413372e909eeb66ed32ce0260c010ed2
Sergii Bugaienko <sergiibugaienko@gmail.com>
2023-08-18 10:32:33 +0200

    HW23 add

---
+++ src/lesson_24/homework23/MainHW23.java
@@ -0,0 +1,26 @@
+package lesson_24.homework23;
+
+public class MainHW23 {
+    public static void main(String[] args) {
+
+        BusDriver driver = new BusDriver("Bob", 33);
+        System.out.println(driver.toString());
+
+        driver.addCategories("C");
+        driver.addCategories("D1");
+        System.out.println(driver.toString());
+
+        System.out.println();
+        System.out.println("==========REMOVE===========");
+
+        System.out.println(driver.removeCategory("D2"));
+        System.out.println(driver.toString());
+
+        System.out.println(driver.removeCategory("B"));
+        System.out.println(driver.removeCategory("C"));
+//        System.out.println(driver.removeCategory("D1"));
+//        driver.removeAllCategories();
+        System.out.println(driver.toString());
+
+    }
+}
